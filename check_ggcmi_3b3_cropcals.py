5dfff533380ca6e4af2ca7d9e137934d1a9f58c4
Sam Rabin <sam.rabin@gmail.com>
2025-05-14 17:48:03 -0600

    Refactor into CropcalFile class.

--- check_ggcmi_3b3_cropcals.py
+++ check_ggcmi_3b3_cropcals.py
@@ -1,11 +1,7 @@
 # %%
-# pylint: disable=invalid-name
 # pylint: disable=missing-module-docstring
 # pylint: disable=missing-function-docstring
 # pylint: disable=missing-class-docstring
-# pylint: disable=too-many-arguments
-# pylint: disable=too-many-positional-arguments
-# pylint: disable=too-many-locals
 # pylint: disable=too-few-public-methods
 
 import os
@@ -89,44 +85,187 @@ class TimeInfo:
         self.dec_ends[-1] = min(self.dec_ends[-1], max(self.years))
 
 
+class CropcalFile:
+    def __init__(self, txtdir, file):
+        self.file = file
+        self.logfile = os.path.join(txtdir, self.file).replace(".nc", ".txt")
+        self.pf = ProblemsFound()
+        self.logger = None
+        self.ds = None
+        self.ok_ranges = None
+        self.timeinfo = None
+
+    def __enter__(self):
+
+        # Open dataset
+        self.open_dataset()
+        self.ok_ranges = get_ok_ranges(self.ds.attrs)
+
+        # Get all decades in this file
+        self.timeinfo = TimeInfo(self.ds)
+
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        """
+        Close the logfile, if it was ever opened
+        """
+        if self.logger:
+            logging.getLogger().removeHandler(self.logger)
+            self.logger.close()
+
+    def process(self):
+        """
+        Loop through and test all variables
+        """
+
+        # Get variable info
+        var_list = list(self.ds.keys())
+        var_list.sort()
+
+        for this_var in var_list:
+            da = self.ds[this_var]
+            self.pf.var = False
+
+            # Check variable for acceptable bounds
+            self.check_var_range(da)
+
+            if self.ds.attrs["crop"] in GS_ALGO_CROP_LIST:
+                # Check variable for constancy within each decade
+                self.check_constancy_within_decades(da)
+                # Check that variable does change over time
+                if this_var not in ["planting_season"]:
+                    self.check_constancy(da, expect_const=False)
+            else:
+                # Check constancy across entire file
+                self.check_constancy(da)
+
+    def warn(self, *args, this_var=None):
+        if not self.pf.file:
+            self.logger = set_up_logger(self.logfile)
+            self.pf.file = True
+            logging.warning(self.file)
+        if this_var is not None and not self.pf.var:
+            self.pf.var = True
+            logging.warning("%s%s", VAR_INDENT, this_var)
+        logging.warning(*args)
+
+    def open_dataset(self):
+        """
+        Open the dataset without decoding times, because cftime has issues with ISIMIP's format
+        """
+        self.ds = xr.open_dataset(self.file, decode_times=False)
+
+        # Set up log file
+        if os.path.exists(self.logfile):
+            os.remove(self.logfile)
+
+        # Process time units
+        time_units_in = self.ds["time"].attrs["units"]
+        if time_units_in == "years since 1601-1-1 00:00:00":
+            new_times = self.ds["time"].values + 1601
+            new_time_da = xr.DataArray(
+                data=new_times.astype(int), dims=["time"], attrs={"units": "year"}
+            )
+            self.ds["time"] = new_time_da
+        else:
+            raise RuntimeError("Unknown time units: " + time_units_in)
+
+        # Get crop info
+        if "Crop" in self.ds.attrs:
+            crop = self.ds.attrs["Crop"][0:3]
+        else:
+            self.warn("%sAttribute 'Crop' missing; getting from filename", VAR_INDENT)
+            crop = os.path.basename(self.file).split("_")[3][0:3]
+        if crop not in CROP_LIST:
+            raise ValueError(f"Unexpected crop: {crop}")
+        self.ds.attrs["crop"] = crop
+        self.ds.attrs["gs_algo"] = crop in GS_ALGO_CROP_LIST
+
+    def check_var_range(self, da):
+        """
+        Check whether variable's data is within acceptable bounds
+        """
+        this_var = da.name
+        ok_range = self.ok_ranges[this_var]
+        if ok_range is None:
+            return
+
+        ok_min = ok_range[0]
+        ok_max = ok_range[1]
+        this_min = np.nanmin(da)
+        this_max = np.nanmax(da)
+        too_low = this_min < ok_min
+        too_high = this_max > ok_max
+        if too_low or too_high:
+            ok_range_str = f"{ok_min}-{ok_max}"
+            this_range_str = f"{this_min}-{this_max}"
+            self.warn(
+                f"{PROBLEM_INDENT}Variable range {this_range_str} outside limits ({ok_range_str})",
+                this_var=this_var,
+            )
+
+    def check_constancy_within_decades(self, da):
+        """
+        Check that data does not vary over time within each decade
+        """
+        for d in np.arange(len(self.timeinfo.dec_ends)):
+            decade = Decade(self.timeinfo, d)
+
+            # Get subset of Dataset for this decade
+            where_this_decade = np.where(
+                (self.timeinfo.years >= decade.start)
+                & (self.timeinfo.years <= decade.end)
+            )[0]
+            if len(where_this_decade) == 0:
+                logging.error("No years found in %s!!!", decade.str)
+            da_decade = da.isel(time=where_this_decade)
+
+            # Check constancy in this decade
+            self.check_constancy(da_decade, decade=decade)
+
+    def check_constancy(self, da, decade=None, expect_const=True):
+        """Check whether a DataArray varies over time
+
+        Args:
+            da (xarray.DataArray): DataArray to check
+            decade (Decade, optional): Decade to check. Defaults to None.
+            expect_const (bool, optional): Whether we expect the variable to be constant over time.
+                                           Defaults to True.
+
+        Raises:
+            NotImplementedError: Haven't yet implemented error message
+        """
+        this_var = da.name
+        mask_min = np.isnan(da).min(dim="time")
+        mask_max = np.isnan(da).max(dim="time")
+        mask_constant = mask_min == mask_max
+        always_nan = mask_constant & (mask_max == 1)
+
+        da_min = da.min(dim="time", skipna=True)
+        da_max = da.max(dim="time", skipna=True)
+        values_constant = (da_min == da_max) | always_nan
+
+        is_not_constant = np.any(~(mask_constant & values_constant))
+        if is_not_constant and expect_const:
+            if decade is not None:
+                msg = get_decade_warning(
+                    decade, mask_constant, da_min, da_max, values_constant
+                )
+                self.warn(msg, this_var=this_var)
+            else:
+                raise NotImplementedError(
+                    "Expected entire file to be constant but it's not; log this"
+                )
+        elif (not is_not_constant) and (not expect_const):
+            self.warn(
+                "%sExpected variation across time but found none",
+                PROBLEM_INDENT,
+                this_var=this_var,
+            )
+
+
 # %% Functions
-
-
-def open_dataset(file, logger, pf, txtdir):
-    """
-    Open the dataset without decoding times, because cftime has issues with ISIMIP's format
-    """
-    ds = xr.open_dataset(file, decode_times=False)
-
-    # Set up log file
-    logfile = os.path.join(txtdir, file).replace(".nc", ".txt")
-    if os.path.exists(logfile):
-        os.remove(logfile)
-
-    # Process time units
-    time_units_in = ds["time"].attrs["units"]
-    if time_units_in == "years since 1601-1-1 00:00:00":
-        new_times = ds["time"].values + 1601
-        new_time_da = xr.DataArray(
-            data=new_times.astype(int), dims=["time"], attrs={"units": "year"}
-        )
-        ds["time"] = new_time_da
-    else:
-        raise RuntimeError("Unknown time units: " + time_units_in)
-
-    # Get crop info
-    if "Crop" in ds.attrs:
-        crop = ds.attrs["Crop"][0:3]
-    else:
-        logger, pf = problem_setup(file, logger, logfile, pf)
-        logging.warning("%sAttribute 'Crop' missing; getting from filename", VAR_INDENT)
-        crop = os.path.basename(file).split("_")[3][0:3]
-    if crop not in CROP_LIST:
-        raise ValueError(f"Unexpected crop: {crop}")
-    ds.attrs["crop"] = crop
-    ds.attrs["gs_algo"] = crop in GS_ALGO_CROP_LIST
-
-    return ds, pf, logfile, logger
 
 
 def get_ok_ranges(attrs):
@@ -164,73 +303,9 @@ def get_ok_ranges(attrs):
     return ok_ranges
 
 
-def check_constancy(
-    file, logger, logfile, pf, this_var, da, decade=None, expect_const=True
-):
-    """Check whether a DataArray varies over time
-
-    Args:
-        file (str): Path to Dataset file
-        logger (logging.FileHandler): For writing to this Dataset's output .txt file
-        logfile (str): Path to this Dataset's output .txt file
-        pf (ProblemsFound)
-        this_var (str): Name of the variable to check
-        da (xarray.DataArray): DataArray to check
-        decade (Decade, optional): Decade to check. Defaults to None.
-        expect_const (bool, optional): Whether we expect the variable to be constant over time.
-                                       Defaults to True.
-
-    Raises:
-        NotImplementedError: Haven't yet implemented error message
+def get_decade_warning(decade, mask_constant, da_min, da_max, values_constant):
     """
-    mask_min = np.isnan(da).min(dim="time")
-    mask_max = np.isnan(da).max(dim="time")
-    mask_constant = mask_min == mask_max
-    always_nan = mask_constant & (mask_max == 1)
-
-    da_min = da.min(dim="time", skipna=True)
-    da_max = da.max(dim="time", skipna=True)
-    values_constant = (da_min == da_max) | always_nan
-
-    is_not_constant = np.any(~(mask_constant & values_constant))
-    if is_not_constant and expect_const:
-        logger, pf = problem_setup(file, logger, logfile, pf, this_var)
-        if decade is not None:
-            log_decade_problem(decade, mask_constant, da_min, da_max, values_constant)
-        else:
-            raise NotImplementedError(
-                "Expected entire file to be constant but it's not; log this"
-            )
-    elif (not is_not_constant) and (not expect_const):
-        logger, pf = problem_setup(file, logger, logfile, pf, this_var)
-        logging.warning(
-            "%sExpected variation across time but found none", PROBLEM_INDENT
-        )
-    return logger, pf
-
-
-def check_var_range(ok_range, pf, file, logfile, logger, this_var, da):
-    """
-    Check whether variable's data is within acceptable bounds
-    """
-    ok_min = ok_range[0]
-    ok_max = ok_range[1]
-    this_min = np.nanmin(da)
-    this_max = np.nanmax(da)
-    too_low = this_min < ok_min
-    too_high = this_max > ok_max
-    if too_low or too_high:
-        # pylint: disable=logging-fstring-interpolation
-        logger, pf = problem_setup(file, logger, logfile, pf, this_var)
-        logging.warning(
-            f"{PROBLEM_INDENT}Variable range {this_min}-{this_max} outside limits ({ok_min}-{ok_max})"
-        )
-    return logger, pf
-
-
-def log_decade_problem(decade, mask_constant, da_min, da_max, values_constant):
-    """
-    Log a warning message when data unexpectedly varies within a decade
+    Get a warning message for when data unexpectedly varies within a decade
     """
     masks_vary = np.any(~mask_constant)
     nonnan_values_vary = np.any(~values_constant)
@@ -245,44 +320,7 @@ def log_decade_problem(decade, mask_constant, da_min, da_max, values_constant):
         if not masks_vary:
             raise RuntimeError("???")
         msg = "Only masks vary"
-    logging.warning("%s%s: %s", PROBLEM_INDENT, decade.str, msg)
-
-
-def check_constancy_within_decades(file, logfile, timeinfo, this_var, da, logger, pf):
-    """
-    Check that data does not vary over time within each decade
-    """
-    for d in np.arange(len(timeinfo.dec_ends)):
-        decade = Decade(timeinfo, d)
-
-        # Get subset of Dataset for this decade
-        where_this_decade = np.where(
-            (timeinfo.years >= decade.start) & (timeinfo.years <= decade.end)
-        )[0]
-        if len(where_this_decade) == 0:
-            logging.error("No years found in %s!!!", decade.str)
-        da_decade = da.isel(time=where_this_decade)
-
-        # Check constancy in this decade
-        logger, pf = check_constancy(
-            file, logger, logfile, pf, this_var, da_decade, decade
-        )
-
-    return logger, pf
-
-
-def problem_setup(file, logger, logfile, pf, this_var=None):
-    """
-    Set up the log file and print initial messages, if needed
-    """
-    if not pf.file:
-        logger = set_up_logger(logfile)
-        pf.file = True
-        logging.warning(file)
-    if this_var is not None and not pf.var:
-        pf.var = True
-        logging.warning("%s%s", VAR_INDENT, this_var)
-    return logger, pf
+    return f"{PROBLEM_INDENT}{decade.str}: {msg}"
 
 
 def set_up_logger(logfile):
@@ -319,57 +357,12 @@ def main(args):
     file_list.sort()
     n_files = len(file_list)
 
-    pf = ProblemsFound()
-
     for f, file in enumerate(file_list):
         print(f"{f+1}/{n_files}: {file}")
 
-        # Open the file
-        pf.file = False
-        logger = None
-        ds, pf, logfile, logger = open_dataset(file, logger, pf, txtdir)
-        ok_ranges = get_ok_ranges(ds.attrs)
-
-        # Get all decades in this file
-        timeinfo = TimeInfo(ds)
-
-        # Loop through all variables
-        var_list = list(ds.keys())
-        var_list.sort()
-        for this_var in var_list:
-            da = ds[this_var]
-            pf.var = False
-
-            # Check variable for acceptable bounds
-            if ok_ranges[this_var] is not None:
-                logger, pf = check_var_range(
-                    ok_ranges[this_var],
-                    pf,
-                    file,
-                    logfile,
-                    logger,
-                    this_var,
-                    da,
-                )
-
-            if ds.attrs["crop"] in GS_ALGO_CROP_LIST:
-                # Check variable for constancy within each decade
-                logger, pf = check_constancy_within_decades(
-                    file, logfile, timeinfo, this_var, da, logger, pf
-                )
-                # Check that variable does change over time
-                if this_var not in ["planting_season"]:
-                    logger, pf = check_constancy(
-                        file, logger, logfile, pf, this_var, da, expect_const=False
-                    )
-            else:
-                # Check constancy across entire file
-                logger, pf = check_constancy(file, logger, logfile, pf, this_var, da)
-
-        # Close the logfile, if it was ever opened
-        if logger:
-            logging.getLogger().removeHandler(logger)
-            logger.close()
+        # Process the file
+        with CropcalFile(txtdir, file) as f:
+            f.process()
 
 
 if __name__ == "__main__":
